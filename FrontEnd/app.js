c05e24fea30cadafb7c45ecad18191128a83f134
Lysenti <eren926@gmail.com>
2024-05-22 17:54:06 +0200

    "Mise à jour Filtres"

--- FrontEnd/app.js
+++ FrontEnd/app.js
@@ -81,59 +81,86 @@ document.addEventListener('DOMContentLoaded', () => {
       const filterDiv = document.querySelector('#portfolio .filter');
       filterDiv.innerHTML = ''; // Efface les filtres existants
 
-      // Ajout du bouton "Tous" pour afficher tous les projets
+      var isAuthenticated = localStorage.getItem('isAuthenticated') === 'true';
+
+     // Si l'utilisateur est authentifié, ne pas afficher les boutons de filtre
+    if (isAuthenticated) {
+        return;
+    }
+
+    // Fonction pour mettre à jour les classes actives des boutons
+    function updateActiveButton(activeButton) {
+        const buttons = filterDiv.querySelectorAll('.filter-button');
+        buttons.forEach(button => {
+            button.classList.remove('active');
+        });
+        activeButton.classList.add('active');
+    }
+
+    // Ajout du bouton "Tous" pour afficher tous les projets
     const allButton = document.createElement('button');
-    allButton.classList.add('filter-button');
+    allButton.classList.add('filter-button', 'active');
     allButton.textContent = 'Tous';
     allButton.addEventListener('click', () => {
-        displayProjects(null); 
+        displayProjects(null);
+        updateActiveButton(allButton);
     });
     filterDiv.appendChild(allButton);
 
-      categories.forEach(category => {
-          const filterButton = document.createElement('button');
-          filterButton.classList.add('filter-button');
-          filterButton.textContent = category.name;
-          filterButton.addEventListener('click', () => {
-              displayProjects(category.id);
-          });
-          filterDiv.appendChild(filterButton);
-      });
-  }
+    // Ajout des boutons de filtre pour chaque catégorie
+    categories.forEach(category => {
+        const filterButton = document.createElement('button');
+        filterButton.classList.add('filter-button');
+        filterButton.textContent = category.name;
+        filterButton.addEventListener('click', () => {
+            displayProjects(category.id);
+            updateActiveButton(filterButton);
+        });
+        filterDiv.appendChild(filterButton);
+    });
+}
   
-  function displayProjects(categoryId = null) {
-      const gallery = document.querySelector('.gallery');
-      gallery.innerHTML = ''; // Efface les projets existants
-      fetch('http://localhost:5678/api/works')
-          .then(response => {
-              if (!response.ok) {
-                  throw new Error('Erreur de récupération des projets.');
-              }
-              return response.json();
-          })
-          .then(data => {
-              const projectsToDisplay = categoryId ?
-                  data.filter(project => project.category.id === categoryId) :
-                  data;
-              projectsToDisplay.forEach(project => {
-                  const projectFigure = document.createElement('figure');
-                  projectFigure.classList.add('category');
-                  projectFigure.setAttribute('id', `project-${project.id}`);
-                  const imageElement = document.createElement('img');
-                  imageElement.src = project.imageUrl;
-                  imageElement.alt = project.title;
-                  projectFigure.appendChild(imageElement);
-                  const figcaptionElement = document.createElement('figcaption');
-                  figcaptionElement.textContent = project.title;
-                  projectFigure.appendChild(figcaptionElement);
-                  gallery.appendChild(projectFigure);
-              });
-          })
-          .catch(error => {
-              console.error('Une erreur s\'est produite lors de la récupération des projets :', error);
-          });
-  }
+function displayProjects(categoryId = null) {
+    const gallery = document.querySelector('.gallery');
+    gallery.innerHTML = ''; // Efface les projets existants
+    fetch('http://localhost:5678/api/works')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Erreur de récupération des projets.');
+            }
+            return response.json();
+        })
+        .then(data => {
+            const projectsToDisplay = categoryId ?
+                data.filter(project => project.category.id === categoryId) :
+                data;
+            projectsToDisplay.forEach(project => {
+                const projectFigure = document.createElement('figure');
+                projectFigure.classList.add('category');
+                projectFigure.setAttribute('id', `project-${project.id}`);
+                const imageElement = document.createElement('img');
+                imageElement.src = project.imageUrl;
+                imageElement.alt = project.title;
+                projectFigure.appendChild(imageElement);
+                const figcaptionElement = document.createElement('figcaption');
+                figcaptionElement.textContent = project.title;
+                projectFigure.appendChild(figcaptionElement);
+                gallery.appendChild(projectFigure);
+            });
+        })
+        .catch(error => {
+            console.error('Une erreur s\'est produite lors de la récupération des projets :', error);
+        });
+}
   
+    // Vérifiez si l'utilisateur est authentifié
+    var authentified = localStorage.getItem('isAuthenticated') === 'true';
+
+    // Vérifiez si l'utilisateur est authentifié
+    if (authentified === true) {
+        addEditModeBanner();
+}
+
   function addEditModeBanner() {
     // Créer la bande noire
     const banner = document.createElement('div');
@@ -156,9 +183,6 @@ document.addEventListener('DOMContentLoaded', () => {
     document.body.prepend(banner);
 }
 
-// Vérifiez si l'utilisateur est authentifié
-if (authentified === true) {
-    addEditModeBanner();
-}
+
 
   
